c61e2bf235b4442e9e688f04f86f0c654e94b214
autumn <cyancirrus@gmail.com>
2025-10-20 17:24:02 -0500

    close

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "pendulum.h"
+// #include "pendulum.h"
 #include <vector>
 #include <btBulletDynamicsCommon.h>
 #include <GL/glew.h>
@@ -8,14 +8,72 @@
 #include <cmath>
 using std::vector;
 
+static constexpr float g = 9.18f;
+static constexpr float l = 1.0f;
+static constexpr float M = 1.0f;
+static constexpr float m = 1.0f;
+
+struct State {
+	float cart_x;
+	float pendulum_x;
+	float pendulum_y;
+};
+
+struct PendulumCart {
+	// angle offset
+	float theta;
+	float theta_dot;
+	// x offset
+	float x;
+	float x_dot;
+
+	PendulumCart();
+	void step(float dt);
+	void control(float dt, float kp, float kd);
+	State position(void);
+};
+
+PendulumCart::PendulumCart() {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	// std::uniform_real_distribution<float> dis(-1.0,1.0);
+	std::uniform_real_distribution<float> dis(-0.1,0.1);
+	theta = dis(gen);
+	theta_dot = 0.0f;
+	x = 0.0f;
+	x_dot = 0.0f;
+}
+
+void PendulumCart::step(float dt) {
+	theta_dot += dt * (M + m) * g /(M * l) * theta;
+	theta += dt * theta_dot;
+	x_dot += dt * (-m * g  * theta ) / M;
+	x += dt * x_dot;
+}
+
+void PendulumCart::control(float dt, float kp, float kd) {
+	float force = -kp * theta -kd * theta_dot;
+	theta_dot += dt * (M + m) * g/(M * l) * theta - force/(M * l);
+	theta += dt * theta_dot;
+	x_dot += dt * (-m * g  * theta + force ) / M;
+	x += dt * x_dot;
+}
+
+State PendulumCart::position(void) {
+	return State { x, x + sin(theta), cos(theta), };
+}
+
+
+
 int main() {
-    // initialize GLFW
+	const float SCALE = 0.25;
+	const float OFFSET = 0.10; // initialize GLFW;
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "StableStar", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1600, 600, "StableStar", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create window\n";
         glfwTerminate();
@@ -24,21 +82,35 @@ int main() {
 
     glfwMakeContextCurrent(window);
 	glClearColor(0, 0, 0, 1);
-	InvPendulum p;
+	PendulumCart p;
 
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		p.step(0.01f);
+		// p.step(0.01f);
 		p.control(0.01f, 20.0f, 6.0f);
+		// p.control(0.01f, 20.0f, 6.0f);
 
-		auto [x, y] = p.position();
+		auto [cart, pendulum_x, pendulum_y] = p.position();
+		cart *= SCALE;
+		pendulum_x *= SCALE;
+		pendulum_y *= SCALE;
+		std::cout << "cart " << cart << " pendulum_x: " << pendulum_x << " pendulum_y " << pendulum_y << "\n";
 
 		glColor3f(1, 0, 1);
 		glLineWidth(10.0f);
+
+		// Box;
+		glBegin(GL_QUADS);
+			glVertex2f(cart + OFFSET, cart + OFFSET);
+			glVertex2f(cart - OFFSET, cart + OFFSET);
+			glVertex2f(cart - OFFSET, cart - OFFSET);
+			glVertex2f(cart + OFFSET, cart - OFFSET);
+		glEnd();
+		// Pendulum
 		glBegin(GL_LINES);
-			glVertex2f(0.0f, 0.0f); // pivot
-			glVertex2f(x, y);       // bob
+			glVertex2f(cart, cart); // pivot
+			glVertex2f(pendulum_x, pendulum_y);       // bob
 		glEnd();
 
 		glfwSwapBuffers(window);
--- src/pendulum.cpp
+++ src/pendulum.cpp
@@ -61,3 +61,43 @@ void simulation(void) {
 		std::cout << p.theta << " " << p.theta_d << "\n";
 	};
 }
+
+// int main() {
+//     initialize GLFW
+//     if (!glfwInit()) {
+//         std::cerr << "Failed to initialize GLFW\n";
+//         return -1;
+//     }
+
+//     GLFWwindow* window = glfwCreateWindow(800, 600, "StableStar", NULL, NULL);
+//     if (!window) {
+//         std::cerr << "Failed to create window\n";
+//         glfwTerminate();
+//         return -1;
+//     }
+
+//     glfwMakeContextCurrent(window);
+// 	glClearColor(0, 0, 0, 1);
+// 	InvPendulum p;
+
+// 	while (!glfwWindowShouldClose(window)) {
+// 		glClear(GL_COLOR_BUFFER_BIT);
+
+// 		p.step(0.01f);
+// 		p.control(0.01f, 20.0f, 6.0f);
+
+// 		auto [x, y] = p.position();
+
+// 		glColor3f(1, 0, 1);
+// 		glLineWidth(10.0f);
+// 		glBegin(GL_LINES);
+// 			glVertex2f(0.0f, 0.0f); // pivot
+// 			glVertex2f(x, y);       // bob
+// 		glEnd();
+
+// 		glfwSwapBuffers(window);
+// 		glfwPollEvents();
+// 	}
+
+// 	return 0;
+// }
