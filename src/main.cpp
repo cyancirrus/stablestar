36ee6e74b988026b9c3ab8a4d184eeb8bb48be0b
autumn <cyancirrus@gmail.com>
2025-10-20 13:21:13 -0500

    interesting, note that like the pde enforces the proportional control term, needs to be bigger than g/l

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,113 @@
 #include <btBulletDynamicsCommon.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <random>
+#include <cmath>
 using std::vector;
 
-struct Pendulum {
+static constexpr float g = 9.18f;
+
+struct InvPendulum {
 	float theta;
-	float theta_dot;
+	float theta_d;
+	// g/l sin(theta)
+	float theta_dd;
+
+	InvPendulum() {
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<float> dis(-0.1,0.1);
+
+		theta = dis(gen);
+		theta_d= 0.0f;
+		theta_dd=0.0f;
+	}
+	void step(float dt) {
+		float l = 1.0f;
+		theta_dd = g/l * sin(theta);
+		theta_d += dt * theta_dd;
+		theta += dt * theta_d;
+	}
+	void control(float dt, float k_p, float k_d) {
+		float l = 1.0f, m = 1.0f;
+		theta_dd = g/l * sin(theta)
+			- k_d / (m * l * l) * theta_d
+			- k_p / (m * l * l) * theta
+		;
+		theta_d += dt * theta_dd;
+		theta += dt * theta_d;
+	}
 };
 
-void step(Pendulum &p, float u, float dt) {
-	float g = 9.18f, l = 1.0f;
-	float theta_dot = (g / l) * sin(p.theta) + u;
-	p.theta += p.theta_dot * dt;
-	p.theta_dot += theta_dot * dt;
-}
 
+// fn ~ c * exp(-wt) + d * exp(wt);
+// consering only the unstable solution as inverted
+//
+// => df/dt = w * d * exp(wt) dt
+// = dt * w * f
 
+// void step(InvPendulum &p, float dt) {
+// 	float g = 9.18f, l = 1.0f;
+// 	p.theta_dd = g/l * sin(p.theta);
+// 	p.theta_d += dt * p.theta_dd;
+// 	p.theta += dt * p.theta_d;
+// }
 
+// void control_step(InvPendulum &p, float k_p, float k_d, float dt) {
+// 	// from pde
+// 	// d/dt d/dt f + kd/ml^2 d/dt f + f (kp/ml^2 -g/l) = 0;
+// 	float g = 9.18f, l = 1.0f, m=1.0f;
+// 	// don't linearize the physic but linearize control
+// 	p.theta_dd = g/l * sin(p.theta)
+// 			- k_d / (m * l * l) * p.theta_d
+// 			- k_p/ (m * l * l) * p.theta;
+
+// 	p.theta_d += dt * p.theta_dd;
+// 	p.theta += dt * p.theta_d;
+// }
 int main(void) {
-	if (!glfwInit()) return -1;
-	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "StableStar", NULL, NULL);
-	if (!window) { glfwTerminate(); return -1; }
-	
-	glfwMakeContextCurrent(window);
-	glewInit();
 
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1, 1, -1, 1, -1, 1);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-	while (!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glColor3f(1.0f, 1.0f, 1.0f);
-			glBegin(GL_LINES);
-			glVertex2f(-0.5f, -0.5f);
-			glVertex2f(0.5f, 0.5f);
-		glEnd();
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+	InvPendulum p;
+	for(int i=0; i<30; i++) {
+		p.control(0.1f, 12.0f, 2.0f);
+		std::cout << p.theta << " " << p.theta_d << "\n";
 	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
 
-	std::cout << "hello world\n";
+
+
+	// if (!glfwInit()) return -1;
+	
+	// GLFWwindow* window = glfwCreateWindow(800, 600, "StableStar", NULL, NULL);
+	// if (!window) { glfwTerminate(); return -1; }
+	
+	// glfwMakeContextCurrent(window);
+	// if (glewInit() != GLEW_OK) {
+	// 	std::cerr << "GLEW INIT FAILED \n";
+	// 	return -1;
+	// }
+
+    // glMatrixMode(GL_PROJECTION);
+    // glOrtho(-1, 1, -1, 1, -1, 1);
+
+	// while (!glfwWindowShouldClose(window)) {
+	// 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    	// glMatrixMode(GL_PROJECTION);
+    	// glLoadIdentity();
+
+	// 	glColor3f(1.0f, 0.0f, 1.0f);
+	// 		glBegin(GL_LINES);
+	// 		// glVertex2f(-0.5f, -0.5f);
+	// 		// glVertex2f(0.5f, 0.5f);
+	// 		glVertex2f(-1.0f, -1.0f);
+	// 		glVertex2f(1.0f, 1.0f);
+	// 	// glEnd();
+
+	// 	glfwSwapBuffers(window);
+	// 	glfwPollEvents();
+	// }
+	// glfwDestroyWindow(window);
+	// glfwTerminate();
+
+	std::cout << "new world\n";
 	return 0;
 }
